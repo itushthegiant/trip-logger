6dbe005ad69a7db55b62dadb3f92d206bb0f96ea
etamar <etamar.levy@gmail.com>
2021-09-25 01:12:04 -0400

    not found response added to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::API
 
-    rescue_from ActiveRecord::RecordInvalid, with: :rescue_from_invalid
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
-    def rescue_from_invalid(invalid)
+
+    
+
+    def render_unprocessable_entity_response(invalid)
         render json: {errors: invalid.record.errors}, status: :unprocessable_entity
     end
+
+    def render_not_found_response
+        render json: { error: "Object not found" }, status: :not_found
+      end
 end
